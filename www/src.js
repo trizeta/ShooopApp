f6765a690946f7db92f4dd6ad55e9e5130d28261
trizeta <michele.rigo@trizeta.com>
2014-10-13 15:46:39 +0200

    win8

--- www/src.js
+++ www/src.js
@@ -301,7 +301,7 @@ require([
 			dojo.connect(registry.byId("tabShowcase"), "onBeforeTransitionIn", null, function() {
                 try{
                     if(showcase) {
-                     
+                        alert("QUIIIIIII:"+tinymce.get("showcasehtmleditor"));
                         tinymce.get("showcasehtmleditor").setContent(showcase.description);                    
                     }
                     domStyle.set('headingshowcase', 'display', 'inline');               
